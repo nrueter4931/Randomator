5408fa6c891d1f25c04043a84419d318a990b0d0
Nick Rueter <masternick4931@mail.fresnostate.edu>
2019-12-04 14:42:03 -0800

    wired all the buttons

--- src/app/main-content/main-content.model.ts
+++ src/app/main-content/main-content.model.ts
@@ -1,9 +1,11 @@
 interface Ipassword {
     generatedPassword: string;
+    edit: boolean;
 }
 
 export class PasswordClass {
     public generatedPassword: string;
+    public edit: boolean;
 
     constructor(thePassword: Ipassword) {
         Object.assign(this, thePassword);
